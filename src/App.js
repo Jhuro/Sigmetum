a251efa5ad5785eadbc567b429c802322c72e686
jupbc <jupbcan100@hotmail.com>
2024-10-23 12:37:54 -0500

    add: navbar, footer, landing page, about us page, explore page sketch version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar.js';
+import Footer from './components/Footer.js';
+import Home from './pages/Home.js';
+import About from './pages/About.js';
+import Explore from './pages/Explore.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+          <Navbar/>
       </header>
-    </div>
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/sobre-nosotros" element={<About/>}/>
+          <Route path="/explorar" element={<Explore/>}/>
+        </Routes>
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+    </Router>
   );
 }
 
